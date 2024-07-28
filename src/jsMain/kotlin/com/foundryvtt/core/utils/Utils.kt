a93ca8c4576c306833d3c241e4c84fe4f066a211
Bernhard Posselt <bernhard@posselt.at>
2024-07-28 11:59:19 +0200

    type utility functions

--- src/jsMain/kotlin/com/foundryvtt/core/utils/Utils.kt
+++ src/jsMain/kotlin/com/foundryvtt/core/utils/Utils.kt
@@ -5,3 +5,7 @@ package com.foundryvtt.core.utils
 import com.foundryvtt.core.AnyObject
 
 external fun expandObject(value: AnyObject): AnyObject
+external fun <T> deepClone(value: T): T
+external fun mergeObject(original: AnyObject, other: AnyObject = definedExternally): AnyObject
+
+
