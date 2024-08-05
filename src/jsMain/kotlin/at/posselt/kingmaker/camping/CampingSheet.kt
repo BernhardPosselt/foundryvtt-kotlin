e8d267c7fc696f14aab856d1365100f3a324e52f
Bernhard Posselt <bernhard@posselt.at>
2024-08-06 01:15:36 +0200

    more prototyping

--- src/jsMain/kotlin/at/posselt/kingmaker/camping/CampingSheet.kt
+++ src/jsMain/kotlin/at/posselt/kingmaker/camping/CampingSheet.kt
@@ -2,12 +2,16 @@ package at.posselt.kingmaker.camping
 
 import at.posselt.kingmaker.app.FormApp
 import at.posselt.kingmaker.app.MenuControl
+import at.posselt.kingmaker.utils.buildPromise
+import com.foundryvtt.core.applications.api.HandlebarsRenderOptions
 import js.core.Void
 import kotlinx.js.JsPlainObject
 import kotlin.js.Promise
 
 @JsPlainObject
-external interface CampingSheetContext
+external interface CampingSheetContext {
+    val size: Array<Int>
+}
 
 @JsPlainObject
 external interface CampingSheetFormData
@@ -18,7 +22,7 @@ external interface CampingSheetFormData
 class CampingSheet : FormApp<CampingSheetContext, CampingSheetFormData>(
     title = "Camping",
     template = "applications/camping/camping-sheet.hbs",
-    width = 960,
+    width = 970,
     classes = arrayOf("km-camping-sheet"),
     controls = arrayOf(
         MenuControl(label = "Show Players", action = "show-players"),
@@ -28,6 +32,14 @@ class CampingSheet : FormApp<CampingSheetContext, CampingSheetFormData>(
         MenuControl(label = "Help", action = "help"),
     )
 ) {
+    override fun _preparePartContext(
+        partId: String,
+        context: CampingSheetContext,
+        options: HandlebarsRenderOptions
+    ): Promise<CampingSheetContext> = buildPromise {
+        CampingSheetContext(size = arrayOf(1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6))
+    }
+
     override fun onParsedSubmit(value: CampingSheetFormData): Promise<Void> {
         TODO("Not yet implemented")
     }
