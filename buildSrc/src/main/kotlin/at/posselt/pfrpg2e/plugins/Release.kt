d56ff81b5fdae575356057937ba6d5a92aa5a7cc
Bernhard Posselt <bernhard@posselt.at>
2024-09-25 23:52:12 +0200

    release task ':release' property 'version'

--- buildSrc/src/main/kotlin/at/posselt/pfrpg2e/plugins/Release.kt
+++ buildSrc/src/main/kotlin/at/posselt/pfrpg2e/plugins/Release.kt
@@ -1,10 +1,8 @@
 package at.posselt.pfrpg2e.plugins
 
 import org.gradle.api.DefaultTask
-import org.gradle.api.file.RegularFile
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
-import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.TaskAction
@@ -23,8 +21,21 @@ abstract class Release : DefaultTask() {
         if (zipFile == null || !zipFile.exists()) {
             throw IllegalStateException("Need an archive file")
         }
-        ProcessBuilder(listOf("git add "))
+        exec(listOf("git", "add", "build.gradle.kts", "module.json"))
+        exec(listOf("git", "commit", "-m", "release $version"))
+        exec(listOf("git", "push", "origin", "master"))
+        exec(listOf("git", "tag", "$version"))
+        exec(listOf("git", "push", "--tags"))
         println(version.get())
         println(zipFile.absolutePath)
+    }
+
+    private fun exec(commands: List<String>) {
+        ProcessBuilder(commands)
+            .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+            .redirectError(ProcessBuilder.Redirect.INHERIT)
+            .directory(project.projectDir)
+            .start()
+            .waitFor()
     }
 }
